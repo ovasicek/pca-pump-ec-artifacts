ebc9eaafdd80b08e3188ffc7f863f5ad26f6d371
ivasicek <ivasicek@fit.vut.cz>
2024-03-27 12:02:31 +0100

    update

--- use_cases_and_exception_cases/cn1.pl
+++ use_cases_and_exception_cases/cn1.pl
@@ -2,3 +2,5 @@ initiallyP(flow_rate_PerHour(6)).
 initiallyP(flow_rate(PerMin)) :- initiallyP(flow_rate_PerHour(PerHour)), PerMin .=. PerHour / 60.
 
 ?- not initiallyP(flow_rate(X)).
+
+%?- initiallyP(flow_rate(X)).
