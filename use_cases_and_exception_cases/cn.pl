534e3e0446888b14242f9150876418a348f3fea7
ivasicek <ivasicek@fit.vut.cz>
2024-03-27 11:51:01 +0100

    update

--- use_cases_and_exception_cases/cn.pl
+++
@@ -1,4 +0,0 @@
-initiallyP(basal_flow_rate_MlperHour(6)).
-initiallyP(basal_flow_rate(MlperMin)) :- initiallyP(basal_flow_rate_MlperHour(MlperHour)), MlperMin .=. MlperHour / 60.
-
-?- not initiallyP(basal_flow_rate(X)).
---
+++ use_cases_and_exception_cases/cn2.pl
@@ -0,0 +1,4 @@
+initiallyP(flow_rate_PerHour(6)).
+-initiallyP(flow_rate(PerMin)) :- initiallyP(flow_rate_PerHour(PerHour)), PerMin .<>. PerHour / 60.
+
+?- -initiallyP(flow_rate(X)).
